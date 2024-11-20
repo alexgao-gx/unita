403a6962e62026b13dd7af7991a3fd021c0737e7
alexgao-gx <gx7182@gmail.com>
2024-11-20 13:06:27 -0800

    update

--- lib/common/components/basic/slider_text.dart
+++ lib/common/components/basic/slider_text.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -18,6 +17,7 @@ class SliderTextWidget extends StatefulWidget {
   final ValueChanged<EnumModel>? onValueChanged;
   final EnumModel? initial;
   final bool? showSliderText;
+
   @override
   State<SliderTextWidget> createState() => _SliderTextWidgetState();
 }
@@ -25,6 +25,7 @@ class SliderTextWidget extends StatefulWidget {
 class _SliderTextWidgetState extends State<SliderTextWidget> {
   List<EnumModel> get _sliderModels => widget.datas ?? <EnumModel>[];
   late double _sliderValue = 0;
+
   @override
   void initState() {
     int index = 0;
@@ -50,58 +51,71 @@ class _SliderTextWidgetState extends State<SliderTextWidget> {
       children: [
         SliderTheme(
           data: const SliderThemeData(
-              tickMarkShape: RoundSliderTickMarkShape(tickMarkRadius: 4.0),
-              trackShape: RectangularSliderTrackShape(),
-              trackHeight: 1.0),
+            tickMarkShape: RoundSliderTickMarkShape(tickMarkRadius: 4.0),
+            trackShape: RectangularSliderTrackShape(),
+            trackHeight: 1.0,
+          ),
           child: Slider(
-              inactiveColor: AppColors.color_C5C5C5,
-              activeColor: AppColors.color_C5C5C5,
-              thumbColor: Colors.white,
-              value: _sliderModels.isNotEmpty
-                  ? min(
-                      1,
-                      max(
-                          0,
-                          _sliderModels.indexWhere(
-                                  (e) => e.isSelected.value == true) /
-                              (_sliderModels.length - 1)))
-                  : 0,
-              divisions: max(1, _sliderModels.length - 1),
-              onChanged: (value) {
-                final divisionCount = max(1, _sliderModels.length - 1);
-                int result = value ~/ (1.0 / divisionCount);
-                for (int i = 0; i < _sliderModels.length; i++) {
-                  _sliderModels[i].isSelected.value = result == i;
-                }
-                _sliderValue = value;
-                widget.onValueChanged?.call(_sliderModels[result]);
-                setState(() {});
-              }).padding(horizontal: 20.w),
+            inactiveColor: AppColors.color_C5C5C5,
+            activeColor: AppColors.color_C5C5C5,
+            thumbColor: Colors.white,
+            value: _sliderModels.isNotEmpty
+                ? min(
+                    1,
+                    max(
+                      0,
+                      _sliderModels.indexWhere(
+                              (e) => e.isSelected.value == true) /
+                          (_sliderModels.length - 1),
+                    ),
+                  )
+                : 0,
+            divisions: max(1, _sliderModels.length - 1),
+            onChanged: (value) {
+              final divisionCount = max(1, _sliderModels.length - 1);
+              int result = value ~/ (1.0 / divisionCount);
+              for (int i = 0; i < _sliderModels.length; i++) {
+                _sliderModels[i].isSelected.value = result == i;
+              }
+              _sliderValue = value;
+              widget.onValueChanged?.call(_sliderModels[result]);
+              setState(() {});
+            },
+          ).padding(horizontal: 20.w),
         ),
         _sliderModels.isNotEmpty && widget.showSliderText == true
             ? Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: _sliderModels.map((model) {
                   return Expanded(
-                    child: TextWidget(
-                      maxLines: null,
-                      softWrap: true,
-                      text: model.text ?? '',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.openSans(
-                        fontSize: 12.sp,
-                        color: model.isSelected.value
-                            ? AppColors.color_65AF7C
-                            : AppColors.color_456C51,
-                        fontWeight: model.isSelected.value
-                            ? FontWeight.w600
-                            : FontWeight.w400,
-                      ),
+                    child: Column(
+                      children: [
+                        FittedBox(
+                          fit: BoxFit.scaleDown,
+                          alignment: Alignment.center,
+                          child: Text(
+                            model.text ?? '',
+                            textAlign: TextAlign.center,
+                            maxLines: 2, // Allow up to 2 lines for wrapping
+                            softWrap: true, // Enable soft wrapping for long text
+                            style: GoogleFonts.openSans(
+                              fontSize: 12.0,
+                              color: model.isSelected.value
+                                  ? AppColors.color_65AF7C
+                                  : AppColors.color_456C51,
+                              fontWeight: model.isSelected.value
+                                  ? FontWeight.w600
+                                  : FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   );
                 }).toList(),
               ).paddingHorizontal(14.w)
             : const SizedBox.shrink(),
+
       ],
     );
   }
